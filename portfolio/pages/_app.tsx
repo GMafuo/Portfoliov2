45a25c0378243a81154023920700b2a3bb890450
GMafuo <matheogirard@hotmail.com>
2025-10-16 14:51:45 +0200

    fix: transition

--- portfolio/pages/_app.tsx
+++ portfolio/pages/_app.tsx
@@ -1,13 +1,23 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 
 import Layout from "../components/Layout";
+import Transition from "../components/Transition";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
